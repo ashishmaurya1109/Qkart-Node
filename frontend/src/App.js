46fc3b7e69635acfc60867649d4e97a325e676c0
ashishmaurya1109 <ashishmaurya1865@gmail.com>
2024-04-19 08:31:36 +0530

    updated backend link

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,7 +15,7 @@ import Thanks from "./components/Thanks";
 // import ipConfig from "./ipConfig.json";
 
 export const config = {
-  endpoint: `https://qkart-backend-ashish.onrender.com/v1`,
+  endpoint: `https://qkart-node-skdx.onrender.com/v1`,
 };
 
 export default function App(props) {
